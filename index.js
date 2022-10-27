df911640cc07f766d5d6a1fa2e182f2ea2d031bb
vinhnguyen2k1 <thanhvinh951@gmail.com>
2022-10-27 23:12:14 +0700

    fix CORS

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import Express from 'express';
 
 const app = Express();
 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.get('/', (req, res) => {
 	res.json({
 		songs: [
